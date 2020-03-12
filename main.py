f20109220f2a024e2dea17f3784fb9da4564bdf3
liweiyap <liweiyap@gmail.com>
2020-03-12 15:55:27 +0000

    Include only commits that have been authored by your_username or your_name

--- main.py
+++ main.py
@@ -17,20 +17,25 @@ def get_user_commits(your_username, my_username, my_password):
             if not repo['fork']:
                 sub_df.loc[repo_idx,'Name'] = repo['name']
                 commits_url = re.sub('{/sha}', '?page=1', repo['commits_url'])
-                sub_df.loc[repo_idx,'Commits'] = get_repo_commits(commits_url, my_username, my_password)
+                sub_df.loc[repo_idx,'Commits'] = get_repo_commits(commits_url, your_username, my_username, my_password)
                 repo_idx += 1
         df = pd.concat([df, sub_df[0:repo_idx]])
         page_idx += 1
         response_repos = requests.get('https://api.github.com/users/%s/repos?page=%d' % (your_username,page_idx), auth=(my_username,my_password))
     return df.sort_values(by=['Commits'], ascending=False)
 
-def get_repo_commits(commits_url, my_username, my_password):
+def get_repo_commits(commits_url, your_username, my_username, my_password):
+    response_user = requests.get('https://api.github.com/users/%s' % your_username, auth=(my_username,my_password))
+    your_name = response_user.json()['name']
+    
     page_idx = 1
     response_commits = requests.get(commits_url, auth=(my_username, my_password))
     commits = json.loads(response_commits.content)
     n_commits = 0
     while (len(commits) != 0):
-        n_commits += len(commits)
+        for commit in commits:
+            if commit['commit']['author']['name'] == your_username or commit['commit']['author']['name'] == your_name:
+                n_commits += 1
         commits_url = re.sub('\?page=%d' % page_idx, '?page=%d' % (page_idx+1), commits_url)
         page_idx += 1
         response_commits = requests.get(commits_url, auth=(my_username, my_password))
